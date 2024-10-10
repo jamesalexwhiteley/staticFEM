6c30c70b00f19d94f762082d4e75dd2e4acc4440
James <james.al.whiteley@gmail.com>
2024-10-10 14:08:15 +0100

    truss.py

---
+++ examples/truss.py
@@ -0,0 +1,33 @@
+import numpy as np 
+from staticFEM.FEM import Truss 
+
+nodes = np.array([
+    [10,10], 
+    [10,0], 
+    [0,10], 
+    [0,0]]) 
+
+elements = np.array([
+            [2,0], 
+            [3,0], 
+            [0,1], 
+            [2,1], 
+            [3,1], 
+            [2,3]]) 
+
+E = 200e9 # Pa 
+A = 0.2 # m2 
+
+truss = Truss(nodes, elements, E, A)
+truss.add_loads(loads=[[0, -100e3],], nodes=[0,])
+truss.add_constraints(dofs=[[1, 1], [1, 0]], nodes=[2, 3])
+truss.add_element_properties(A=0.4, elems=(0, 2, 4, 5)) 
+
+truss.initialise()  
+truss.solve()
+truss.show(member_id=True, 
+           node_id=False, 
+           supports=True, 
+           nodal_forces=True, 
+           nodal_disp=False)
+
